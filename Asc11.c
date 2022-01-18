71c29ef2bab9e3574c7d33f7edb409b62d571cd3
Xuhaoran <xhr1138743695@gmail.com>
2022-01-18 11:17:58 +0800

    main

---
+++ Asc11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+{   char character;
+
+    printf("Please enter your words:");
+
+    scanf("%c",&character);
+
+    printf("The ascii is %d",character);
+
+    return 0;
+
+
+
+}
